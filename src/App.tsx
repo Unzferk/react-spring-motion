20e1c4de549ce308448b9918f5d0540e22bb56cc
Fernando Unzueta <rudy.unzueta@truextend.com>
2022-12-16 23:15:46 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 
-function App() {
+import styles from "./styles.module.css";
+import Presentation from "./components/Presentation";
+// Little helpers ...
+const url = (name: string, wrap = false) =>
+  `${
+    wrap ? "url(" : ""
+  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
+    wrap ? ")" : ""
+  }`;
+
+export default function App() {
+  const parallax = useRef<IParallax>(null!);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.container}>
+      <Parallax ref={parallax} pages={4}>
+        <ParallaxLayer
+          offset={1}
+          speed={1}
+          style={{ backgroundColor: "#805E73" }}
+        />
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          style={{ backgroundColor: "#87BCDE" }}
+        />
+        <ParallaxLayer
+          offset={3}
+          speed={1}
+          style={{ backgroundColor: "red" }}
+        />
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={3}
+          style={{
+            backgroundImage: url("stars", true),
+            backgroundSize: "cover",
+          }}
+        />
+
+        <ParallaxLayer
+          offset={1.3}
+          speed={-0.3}
+          style={{ pointerEvents: "none" }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={url("satellite4")}
+            style={{ width: "15%", marginLeft: "70%" }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={0}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            pointerEvents: "none",
+          }}
+        >
+          <img src={url("earth")} style={{ width: "60%" }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0.3}
+          style={{
+            backgroundSize: "80%",
+            backgroundPosition: "center",
+            backgroundImage: url("clients", true),
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(1)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Presentation />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src={url("bash")} style={{ width: "40%" }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={() => parallax.current.scrollTo(0)}
+        >
+          <img src={url("clients-main")} style={{ width: "40%" }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={-0}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={() => parallax.current.scrollTo(0)}
+        >
+          <img src={url("bash")} style={{ width: "40%" }} />
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Presentation.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import logo from "../logo.svg";
+import { animated, useSpring } from "@react-spring/web";
+import { useTransition } from "@react-spring/web";
+import "../App.css";
+import styles from "../styles.module.css";
+
+const Presentation: React.FC = () => {
+  const ref = useRef<ReturnType<typeof setTimeout>[]>([]);
+  const [items, set] = useState<string[]>([]);
+  const transitions = useTransition(items, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: "perspective(600px) rotateX(0deg)",
+      color: "#8fa5b6",
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: "perspective(300px) rotateX(180deg)", color: "#28d79f" },
+      { transform: "perspective(300px) rotateX(0deg)" },
+    ],
+    leave: [
+      { color: "#c23369" },
+      { innerHeight: 0 },
+      { opacity: 0, height: 0 },
+    ],
+    update: { color: "#28b4d7" },
+  });
+
+  const reset = useCallback(() => {
+    ref.current.forEach(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(
+      setTimeout(() => set(["Simple", "Page", "Application"]), 2000)
+    );
+    ref.current.push(setTimeout(() => set(["Simple", "Application"]), 5000));
+    ref.current.push(
+      setTimeout(() => set(["Simple", "Page", "Application"]), 8000)
+    );
+  }, []);
+
+  useEffect(() => {
+    reset();
+    return () => ref.current.forEach(clearTimeout);
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.leftcont}>
+        <div>
+          {transitions(({ innerHeight, ...rest }, item) => (
+            <animated.div
+              className={styles.transitionsItem}
+              style={rest}
+              onClick={reset}
+            >
+              <animated.div style={{ overflow: "hidden", height: innerHeight }}>
+                {item}
+              </animated.div>
+            </animated.div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.rightcont}>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Presentation;
